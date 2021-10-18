01380f5df48a512c57b8306d618cc552257bd741
db2dinosaur <james.gill@triton.co.uk>
2021-10-18 21:02:36 +0100

    Update SubmitAndTrack.groovy

--- vars/SubmitAndTrack.groovy
+++ vars/SubmitAndTrack.groovy
@@ -5,14 +5,13 @@
     JCLDatasetName  : MVS dataset name for the JCL to submit
     MaxCC           : Max job CC for success
     MaxWait         : Max wait time for job to complete
-    MaxFileSize     : Max # lines to retrieve for an output file
     DDList          : Array files to return (proc.step.ddname)
   Returns:
     <OK|FAIL> <Message>
     JESMSGLG
     Requested output DDs
 */
-def call(String jcldsn,def maxcc,def maxwait,def maxfilesize,String[] ddlist) {
+def call(String jcldsn,long maxcc,int maxwait,String[] ddlist) {
   /* start timer */
   Date start = Date();
   /* submit the job */
@@ -22,6 +21,8 @@ def call(String jcldsn,def maxcc,def maxwait,def maxfilesize,String[] ddlist) {
   Boolean endofjob = false;
   String status = "";
   String[] st;
+  status = CheckJob(jobn);
+  st = status.tokenize(" ");
   /* wait for it to finish or timeout */
   do while ((Date().GetTime() <= deadline) && !endofjob) {
     status = CheckJob(jobn);
@@ -34,16 +35,21 @@ def call(String jcldsn,def maxcc,def maxwait,def maxfilesize,String[] ddlist) {
     }
   }
   if (endofjob) {
-    // CHECK CC / ABEND / ETC HERE!
-    rmsg = "OK";
-    rmsg += "\n" + GetDDNameJob(jobn,"JES2","JES2MSGLG");
-    for (String ddprst in ddlist) {
-      String[] parts = ddprst.split("\\.");
-      if (parts.size() < 3) {
-        rmsg += "\n" + GetDDNameJob(jobn,parts[0],parts[1]);
-      } else {
-        rmsg += "\n" + GetDDNameJob(jobn,"${parts[0]}.${parts[1]}",parts[2]);
+    // CHECK CC
+    if ((st[3] == "CC") && (st[4].toInteger() <= maxCC)) {
+      rmsg = "OK";
+      rmsg += "\n" + GetDDNameJob(jobn,"JES2","JES2MSGLG");
+      for (String ddprst in ddlist) {
+        String[] parts = ddprst.split("\\.");
+        if (parts.size() < 3) {
+          rmsg += "\n" + GetDDNameJob(jobn,parts[0],parts[1]);
+        } else {
+          rmsg += "\n" + GetDDNameJob(jobn,"${parts[0]}.${parts[1]}",parts[2]);
+        }
       }
+    } else {
+      rmsg = "FAIL ${st[3]} = ${st[4]}";
+      rmsg += "\n" + GetDDNameJob(jobn,"JES2","JES2MSGLG");
     }
   } else {
     rmsg = "FAIL Job still running after ${maxwait} seconds";
