0578e00020ed2b190fcd6c1d71abc92ae8959399
db2dinosaur <james.gill@triton.co.uk>
2021-10-18 17:18:44 +0100

    Create GetDDNameJob.groovy

---
+++ vars/GetDDNameJob.groovy
@@ -0,0 +1,19 @@
+#!groovy
+
+/* This function returns the output for a DDname from a job
+*/
+
+def call(def jobNum, def stepName, def ddName) {
+  def procstep = stepName.split("\\.");
+  def proc = "";
+  def step = "";
+  if (procstep.size > 1) {
+    proc = procstep[0];
+    step = procstep[1];
+  } else {
+    proc = "";
+    step = procstep[0];
+  }
+  def ddfile = sh(returnStdout: true, script: "pjdd ${jobNum} ${proc} ${step} ${ddName}").trim();
+  return ddfile;
+}
