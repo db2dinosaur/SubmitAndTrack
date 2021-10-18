40449ab91d65ed25c41c5769c5b0e6840dd31477
db2dinosaur <james.gill@triton.co.uk>
2021-10-18 15:49:34 +0100

    Update DDNamesJob.groovy

--- vars/DDNamesJob.groovy
+++ vars/DDNamesJob.groovy
@@ -2,8 +2,8 @@
 
 /* This function returns a list of DD names available for download for a job:
      <step>  <proc>  <ddname>  <type>  <lrecl>  <#recs>
-   Where: <step> is either JES2 for messages from JES2, or the job step name
-          <proc> is the JCL proc name (if used), or "-"
+   Where: <procstep> is either JES2 for messages from JES2, or the job step name if <step> is "-", or the step name
+          <step> is the JCL proc step name (if used), or "-"
           <type> is the data type - e.g. VB, VBA, or some weird ones for JES2
           <lrecl> is the logical (max) record length
           <#recs> is the number of records in the file
