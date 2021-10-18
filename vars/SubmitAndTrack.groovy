0f62c40b6724e37697006bf74864338225b665dc
db2dinosaur <james.gill@triton.co.uk>
2021-10-18 22:32:08 +0100

    Update SubmitAndTrack.groovy

--- vars/SubmitAndTrack.groovy
+++ vars/SubmitAndTrack.groovy
@@ -63,11 +63,11 @@ def call(String jcldsn,int maxcc,int maxwait,String[] ddlist) {
     } else {
       rmsg = "FAIL ${st[3]} = ${st[4]}";
       rmsg += "\n" + GetDDNameJob(jobn,"JES2","JESMSGLG");
-      currentBuild.result = "FAILURE";
+      error rmsg;
     }
   } else {
     rmsg = "FAIL Job still running after ${maxwait} seconds";
-    currentBuild.result = "FAILURE";
+    error rmsg;
   }
   return rmsg;
 }
