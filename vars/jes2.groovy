e767f89b1a69327f3d4639ad8e8509b136d4d3f6
db2dinosaur <james.gill@triton.co.uk>
2021-10-20 14:45:39 +0100

    Update and rename SubmitJ.groovy to jes2.groovy

---
+++ vars/jes2.groovy
@@ -0,0 +1,18 @@
+/*
+ * jes2 class used to submit and track jobs
+ * file name = jes2.groovy
+ */
+import com.ibm.zoautil.Jobs;
+import com.ibm.zoautil.JobsOptions;
+
+package com.triton.zos
+
+public class jes2 {
+  
+  String sub(String jclDataset) {
+    String jobn = "--FAIL--";
+    jobn = Jobs.submit(jclDataset);
+    return jobn;
+  }
+
+}
